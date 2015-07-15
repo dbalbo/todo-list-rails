035018316065dcdf5f00d832b866e8dd8a9cf782
debbie <debbalbo@hotmail.com>
2015-07-15 12:31:26 -0700

    all is functioning properly for lists paths. On to tasks next

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,14 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
   end
+
   def show
     @list = List.find(params[:id])
-    render :show
   end
 
   def new
     @list = List.new
-    render :new
   end
 
   def create
@@ -21,16 +19,26 @@ class ListsController < ApplicationController
       render :new
     end
 
+    def edit
+      @list = List.find(params[:id])
+    end
+
     def update
       @list = List.find(params[:id])
-      if@list.update(list_params)
+      if @list.update(list_params)
+        flash[:notice] ="Boom! you updated this correctly! Go Have Beers!"
         redirect_to lists_path
       else
         render :edit
       end
     end
 
-
+    def destroy
+      @list = List.find(params[:id])
+      @list.destroy
+      flash[:notice] = "List was DESTROYED"
+      redirect_to lists_path
+    end
   end
 
   private
